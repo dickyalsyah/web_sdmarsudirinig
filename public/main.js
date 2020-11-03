e8f27593fa430c17aead995e62b4fbc4ccec4565
dickyalsyah <dickyalsyah@gmail.com>
2020-11-04 02:21:39 +0700

    fixing bug

--- public/main.js
+++ public/main.js
@@ -4,6 +4,7 @@ $(function () {
     var logoShape = $("#logo-nav");
     var imgLogo = $("#img-logo");
     var subMenu = $("ul#sub-menu");
+    var namaSekolah = $("#nama-sekolah");
 
     $(window).scroll(function () {
         var scroll = $(window).scrollTop();
@@ -16,6 +17,8 @@ $(function () {
         imgLogo.removeClass("w-auto p-2 lg:p-3");
         subMenu.removeClass("bg-blue-800");
         subMenu.addClass("text-pri-100");
+        namaSekolah.addClass("block")
+        namaSekolah.removeClass("hidden")
         } else {
         navigation.addClass("text-white");
         navigation.removeClass("bg-white xl:py-4 shadow-md text-pri-100");
@@ -25,6 +28,8 @@ $(function () {
         logoShape.addClass("bg-white shadow-xl rounded-b-xl");
         subMenu.addClass("bg-blue-800");
         subMenu.removeClass("text-pri-100");
+        namaSekolah.removeClass("block");
+        namaSekolah.addClass("hidden");
         }
     });
 });
