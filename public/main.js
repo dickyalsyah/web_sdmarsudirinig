972e9e3b3c9bedf8bf2b59e6d159dad5e8bf2d2a
dickyalsyah <dickyalsyah@gmail.com>
2020-11-12 21:14:48 +0700

    revision

--- public/main.js
+++ public/main.js
@@ -12,7 +12,7 @@ $(function () {
         navigation.addClass("bg-white shadow-md text-pri-100");
         navigation.removeClass("text-white");
         nav.removeClass("bg-pri-100")
-        logoShape.removeClass("bg-white shadow-xl");
+        // logoShape.removeClass("bg-white shadow-xl");
         imgLogo.addClass("w-16 py-2");
         imgLogo.removeClass("w-auto p-2 lg:p-3");
         subMenu.removeClass("bg-blue-800");
@@ -25,7 +25,7 @@ $(function () {
         nav.addClass("bg-pri-100")
         imgLogo.removeClass("w-16 py-2");
         imgLogo.addClass("w-auto p-2 lg:p-3");
-        logoShape.addClass("bg-white shadow-xl rounded-b-xl");
+        // logoShape.addClass("bg-white shadow-xl rounded-b-xl");
         subMenu.addClass("bg-blue-800");
         subMenu.removeClass("text-pri-100");
         namaSekolah.removeClass("block");
